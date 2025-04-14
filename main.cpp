078d90e14cc7c0372d21419b28a059c731c5c896
Dinar <MullagalievDR@nexteng.ru>
2025-04-14 17:06:57 +0300

    Наебашил я крч эту залупу. Единственное, надо сделать хуйню с игроками, чтобы у каждого был свой знак и чё-то ещё, но я уже забыл
    Signed-off-by: Dinar <MullagalievDR@nexteng.ru>

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
+// UsProject.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
 #include <iostream>
+#include <vector>
 
-using namespace std;
+int lineWriting()
+{
+    std::cout << "Write line" << std::endl;
+    int line;
+    std::cin >> line;
+    return line;
+}
+int columnWriting()
+{
+    std::cout << "Write column" << std::endl;
+    int column;
+    std::cin >> column;
+    return column;
+}
+void conclusionMassiv(int mline, int mcolumn, std::vector<std::vector<int>>& MassivOutMain)
+{
+    MassivOutMain[mline][mcolumn] = 1;
 
-int main() {
-cout << "Hello Паша \n";
+    for (size_t i = 0; i < MassivOutMain.size(); ++i) {
+        for (size_t j = 0; j < MassivOutMain[i].size(); ++j) {
+            std::cout << MassivOutMain[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    int i = 0;
+
+    std::vector<std::vector<int>> mainmassiv = { { 3, 3, 3 }, {3, 3, 3}, { 3, 3, 3 } };
+
+    do {
+        int line = lineWriting();
+        int column = columnWriting();
+        conclusionMassiv(line, column, mainmassiv);
+        ++i;
+    } while (i <= 9);
 }
